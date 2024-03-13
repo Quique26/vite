c5a625c08378747c501f3025e964e232066f3c9b
Quique26 <luis.nunez@cuidum.com>
2024-03-13 11:26:11 +0100

    ficheros de vite y ts

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { SizeSelector } from './components/SizeSelector'
+import './App.css'
+
+
+function App() {
+  
+  const [currentSize, setCurrentSize] = useState('none')
+
+  return (
+    <div className='App App-header'>
+      <h1>Talla seleccionada: { currentSize }</h1>
+      <SizeSelector 
+        selectedSize={ currentSize } 
+        onSizeChange={ (size) => setCurrentSize(size)}
+      />
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SizeSelector.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+
+const possiblesSizes = ['XS','S','M','L','XL','XXL'];
+
+interface Props{
+    selectedSize: string;
+    onSizeChange: ( size: string) => void;
+}
+
+const renderOptions = () => {
+    return possiblesSizes.map((size) => (
+        <option key={size} value={size}>
+            {size}
+        </option>
+    ));
+};
+
+export const SizeSelector:FC<Props> = ({ selectedSize, onSizeChange }) => {
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const newSize = event.target.value;
+        onSizeChange(newSize);
+    };
+    return (
+        
+        <div style={{display: 'flex'}}>
+            {
+                possiblesSizes.map( size => (
+                    <button key={ size } className = { selectedSize === size ? 'active': '' } onClick={ () => onSizeChange( size ) }>
+                        { size }
+                    </button>
+                ))
+            }
+            <div style={{ display: 'flex' }}> 
+            <select id="size" value={ selectedSize } onChange={ handleChange }>
+                {
+                    renderOptions()    
+                }
+                </select>
+            </div>
+        </div>
+    )
+}
+
